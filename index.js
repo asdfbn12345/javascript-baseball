7ed382bc00b5830f466b7a43d292d1415bd908fe
asdfbn12345 <asdfbn12345@gmail.com>
2024-11-13 17:05:02 +0900

    바톤 터치!

---
+++ index.js
@@ -0,0 +1,15 @@
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input : process.stdin,
+//     output : process.stdout,
+// });
+
+// let input = [];
+
+// rl.on('line', (line) => {
+//     rl.close();
+// });
+
+
+let userInput = prompt("이름을 입력하세요");
+console.log(userInput);
